3d3846ea4b83ce5375906b75d262210cc73036b0
Yelyzaveta Demchenko <demcenkoelizaveta6@gmail.com>
2024-07-18 15:25:05 +0200

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import Providers from '@/app/components/providers';
+import './globals.css';
+
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-xl">Home page</h1>
+    </main>
+  );
+}
